abbc8b41d295d286c9890b064c81b720e7d439a3
Asher Beck <117310101+Asherbeck10@users.noreply.github.com>
2023-02-23 20:00:36 +0000

    added push team

--- index.js
+++ index.js
@@ -11,12 +11,20 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./src/page-template.js");
 
 
-// TODO: Write Code to gather information about the development team members, and render the HTML file.
+// Code to gather information about the development team members.
 
+
+
+
+
+//render the HTML file
 let team=[];
 writeToHTML()
 async function writeToHTML(){
 team.push(new Manager("Bob",001,"bob@g.com",1001))
+team.push(new Engineer("John",002,"john@g.com","john10"))
+team.push(new Intern("Erik",020,"e@g.com","The coding school"))
 let htmlDoc=render(team)
 await fs.writeFile("index.html",htmlDoc)
 }
+console.log(team)
