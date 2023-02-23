d86c9ccfbeacc2163b3e22d30fd727ae9928eed7
Asher Beck <117310101+Asherbeck10@users.noreply.github.com>
2023-02-23 18:55:01 +0000

    added inquuirer and html first attempt

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
-const fs = require("fs");
+const fs = require("fs/promises");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
@@ -13,3 +13,10 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+let team=[];
+writeToHTML()
+async function writeToHTML(){
+team.push(new Manager("Bob",001,"bob@g.com",1001))
+let htmlDoc=render(team)
+await fs.writeFile("index.html",htmlDoc)
+}
